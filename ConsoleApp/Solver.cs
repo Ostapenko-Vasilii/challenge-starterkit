9305146da86e82cbdde139fc0a2f40bb3d727a60
ivk <email@site.com>
2026-07-07 19:36:57 +1000

    Solver demo

--- ConsoleApp/Solver.cs
+++ ConsoleApp/Solver.cs
@@ -1,4 +1,6 @@
 using Challenge.DataContracts;
+using System;
+using System.Linq;
 
 namespace ConsoleApp;
 
@@ -6,6 +8,23 @@ public class Solver
 {
     public static string Solve(TaskResponse taskResponse)
     {
-        return "42";
+        if (taskResponse.TypeId == "cypher")
+        {
+            var a = taskResponse.Question.Split("#");
+            if (a[1].Equals("reversed"))
+            {
+                char[] charArray = a[2].ToCharArray(); 
+                Array.Reverse(charArray);
+                string reversed = new string(charArray);
+
+                return reversed;
+            }
+            else
+            {
+                throw new Exception("Шифр не reversed");
+            }
+            
+        }
+        return "24";
     }
 }
