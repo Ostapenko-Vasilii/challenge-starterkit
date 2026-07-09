2d487dba44dd6085ff1e9b78676a99abb7133e13
Ostapenko-Vasilii <vasiliiostapenko25@gmail.com>
2026-07-09 13:02:46 +0600

    refactor Solver.cs

--- ConsoleApp/Solver.cs
+++ ConsoleApp/Solver.cs
@@ -8,69 +8,10 @@ public class Solver
 {
     public static string Solve(TaskResponse taskResponse)
     {
-        if (taskResponse.TypeId == "cypher")
+        switch (taskResponse.TypeId)
         {
-            var a = taskResponse.Question.Split("#");
-            if (a[1].Equals("reversed"))
-            {
-                char[] charArray = a[2].ToCharArray(); 
-                Array.Reverse(charArray);
-                string reversed = new string(charArray);
-
-                return reversed;
-            }
-            else
-            {
-                throw new Exception("Шифр не reversed");
-            }
-            
         }
-        if (taskResponse.TypeId == "determinant")
-        {
-            long[,] matrix = ParseMatrix(taskResponse.Question);
-            long answer = FindDeterminant(matrix);
-
-            return answer.ToString();
-        }
-        return "24";
+        throw  new NotImplementedException();
     }
-    private static long[,] ParseMatrix(string question){
-        string[] rows = question.Split(@"\\");
-        int size = rows.Length;
-        long[,] matrix = new long[size, size];
-        for (int i = 0; i < size; i++){
-            string[] elements = rows[i].Split("&");
-            for (int j = 0; j < size; j++){
-                matrix[i, j] = long.Parse(elements[j].Trim());
-            }
-        }
-        return matrix;
-    }
-    private static long FindDeterminant(long[,] matrix)
-    {
-        int n = matrix.GetLength(0);
-        if (n == 1)
-            return matrix[0, 0];
-        if (n == 2)
-            return matrix[0, 0] * matrix[1, 1] - matrix[0, 1] * matrix[1, 0];
-
-        long determinant = 0;
-        for (int p = 0; p < n; p++)
-        {
-            long[,] subMatrix = new long[n - 1, n - 1];
-            for (int i = 1; i < n; i++)
-            {
-                int subColIndex = 0;
-                for (int j = 0; j < n; j++)
-                {
-                    if (j == p)
-                        continue;
-                    subMatrix[i - 1, subColIndex] = matrix[i, j];
-                    subColIndex++;
-                }
-            }
-            determinant += matrix[0, p] * FindDeterminant(subMatrix) * (p % 2 == 0 ? 1 : -1);
-        }
-    return determinant;
-    }
+  
 }
