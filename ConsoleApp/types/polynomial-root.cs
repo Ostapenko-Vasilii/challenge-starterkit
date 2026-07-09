da1e0d05d18f21920fe3ae1804a317059254edfd
Danil <danilgorobchuk@mail.ru>
2026-07-09 13:23:51 +0500

    Add polynomial root parser

--- ConsoleApp/types/polynomial-root.cs
+++ ConsoleApp/types/polynomial-root.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Globalization;
 using Challenge.DataContracts;
 
 namespace ConsoleApp.types;
@@ -7,6 +8,58 @@ public static class PolynomialRoot
 {
     public static string Solve(TaskResponse taskResponse)
     {
-        throw new NotImplementedException();
+        var coefficients = ParsePolynomial(taskResponse.Question);
+
+        double a = coefficients.a;
+        double b = coefficients.b;
+        double c = coefficients.c;
+
+        return "no roots";
+    }
+     private static (double a, double b, double c) ParsePolynomial(string question)
+    {
+        double a = 0;
+        double b = 0;
+        double c = 0;
+
+        string[] parts = question.Split('+');
+
+        foreach (string rawPart in parts)
+        {
+            string part = rawPart.Trim();
+
+            if (part.Contains("x^2"))
+            {
+                a = GetCoefficient(part, "*x^2");
+            }
+            else if (part.Contains("x"))
+            {
+                b = GetCoefficient(part, "*x");
+            }
+            else
+            {
+                c = ParseNumber(part);
+            }
+        }
+
+        return (a, b, c);
+    }
+    private static double GetCoefficient(string part, string variablePart)
+    {
+        string coefficient = part.Replace(variablePart, "").Trim();
+
+        return ParseNumber(coefficient);
+    }
+
+    private static double ParseNumber(string value)
+    {
+        value = value.Trim();
+
+        if (value.StartsWith("(") && value.EndsWith(")"))
+        {
+            value = value.Substring(1, value.Length - 2);
+        }
+
+        return double.Parse(value, CultureInfo.InvariantCulture);
     }
 }
